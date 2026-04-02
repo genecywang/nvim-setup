d16464321cb4095a4e0596f03ce5de7ed754a28c
genecywang <wanggiyans@gmail.com>
2026-04-03 03:51:48 +0800

    feat: enable OSC52 clipboard for SSH environments

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -31,6 +31,19 @@ opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or 
 -- clipboard
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
+-- OSC52: enable clipboard over SSH via terminal escape sequence
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+
 -- split windows
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
